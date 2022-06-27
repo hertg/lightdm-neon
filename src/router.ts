da9e42fb771f4ac0f460fafdfaa0bef59601f767
Michael <3533648+hertg@users.noreply.github.com>
2022-06-27 23:56:34 +0200

    rename WMSelect to Sessions

--- src/router.ts
+++ src/router.ts
@@ -2,14 +2,14 @@ import Splash from './pages/Splash.svelte';
 import UserSelection from './pages/UserSelection.svelte';
 import PasswordPrompt from './pages/PasswordPrompt.svelte';
 import UserPrompt from './pages/UserPrompt.svelte';
-import WmSelect from './pages/WMSelect.svelte';
 import PowerMenu from './pages/PowerMenu.svelte';
+import Sessions from './pages/Sessions.svelte';
 
 export default {
     '/': Splash,
     '/select-user': UserSelection,
     '/login': PasswordPrompt,
     '/user': UserPrompt,
-    '/session': WmSelect,
+    '/session': Sessions,
     '/power-menu': PowerMenu,
 };
